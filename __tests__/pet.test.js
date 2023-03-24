737e08cb4946f04460923b736bdac34bcdb8b9d7
u1986f <u1986f@gmail.com>
2023-03-24 09:38:50 +0000

    Kalb's growingUp

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,5 +13,21 @@ describe('constructor', () => {
       expect(pet.name).toEqual('Kalb');
     });
   });
+  
+  describe('constructor', () => {
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Kalb');
+  
+      expect(pet.age).toEqual(0);
+    });
+  });
 
-});
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Kalb');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
+    });
+  });
