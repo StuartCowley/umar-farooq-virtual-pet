e4d7bc8e021382c9d0827fb858b4db64f57a314e
u1986f <u1986f@gmail.com>
2023-03-24 09:15:12 +0000

    updating this.name

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,17 @@
-const Pet = require('../src/pet');
+const Pet = require('../src/pet.js');
 
 describe('constructor', () => {
   it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+    expect(new Pet('Kalb')).toBeInstanceOf(Object);
   });
+});
+
+  describe('constructor', () => {
+    it('sets the name property', () => {
+      const pet = new Pet('Kalb');
+  
+      expect(pet.name).toEqual('Kalb');
+    });
+  });
+
 });
