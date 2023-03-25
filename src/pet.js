1a041503c3d6f5dbc10cd26b27b8f8572401ece3
u1986f <u1986f@gmail.com>
2023-03-25 13:15:25 +0000

    updating pet

--- src/pet.js
+++ src/pet.js
@@ -8,4 +8,12 @@ function Pet(name) {
     };
   }
 
+  Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= 10 ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = 10;
+    }
+  }
+
 module.exports = Pet;
