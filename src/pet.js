ccbd7eb224f78f210526b26ea7cb2614d579ebf3
u1986f <u1986f@gmail.com>
2023-03-25 11:20:42 +0000

    "Updated hunger and fitness"

--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,8 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.hunger = MINIMUM_HUNGER;
-    this.fitness = FITNESS_START;
+    this.hunger = 0;
+    this.fitness = 10;
     this.growUp = function() {
       this.age += 1;
     };
