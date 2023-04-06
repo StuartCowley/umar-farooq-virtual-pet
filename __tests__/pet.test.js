e5d3f255a4a829b41c9f23c7e9d6a1bda7e31aba
u1986f <u1986f@gmail.com>
2023-04-06 18:33:11 +0100

    ensuring all tests pass (amending syntax)

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,12 +7,14 @@ describe('constructor', () => {
 
   it('sets the name property', () => {
     const pet = new Pet('Kalb');
+
     expect(pet.name).toEqual('Kalb');
     });
 
-    it('has a initial age of 0', () => {
+    it('has an initial age of 0', () => {
       const pet = new Pet('Kalb');
-  
+      pet.growUp();
+
       expect(pet.age).toEqual(1);
     });
 
@@ -34,8 +36,7 @@ describe('constructor', () => {
   describe('walk', () => {
     it('increases fitness by 4', () => {
       const pet = new Pet('Kalb');
-
-      pet.fitness = 8;
+      pet.fitness = 4;
       pet.walk();
 
       expect(pet.fitness).toEqual(8);
@@ -43,21 +44,39 @@ describe('constructor', () => {
 
     it('increases fitness to a maximum of 10', () => {
       const pet = new Pet('Kalb');
-      pet.fitness = 4;
+      pet.fitness = 8;
       pet.walk();
 
-      expect(pet.fitness).toEqual(8);
+      expect(pet.fitness).toEqual(10);
     });
 
     it('throws an error if the pet is not alive', () => {
     const pet = new Pet('Kalb');
     pet.age = 30;
 
-    expect (() => pet.walk()).toThrow('Your pet is no longer alive :(');
+    expect (() => pet.walk()).toThrow('Your pet is no longer aliv');
   });
 });
 
+describe('grow up', () => {
+  it('increases the age of the pet in increaments of 1', () => {
+    const pet = new Pet('Kalb');
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  })
+
+  it('throws an error if the pet is not alive', () => {
+  const pet = new Pet('Kalb');
+  pet.age = 30;
+
+  expect(() => pet.growUp()).toThrow('Your pet is no longer alive!');
+  });
+});
+
+
 describe('feed', () => {
+
   it('prevents hunger level from going below 0', () => {
     const pet = new Pet('Kalb');
     pet.hunger = 1;
@@ -78,28 +97,14 @@ describe('feed', () => {
   const pet = new Pet('Kalb');
   pet.age = 30
 
-  expect(pet.feed()).toThrow('Your pet is no longer alive!')
+  expect(() => pet.feed()).toThrow('Your pet is no longer alive')
   });
 });
 
-describe('grow up', () => {
-  it('increases the age of the pet in increaments of 1', () => {
-    const pet = new Pet('Kalb');
-    pet.growUp();
-
-    expect(pet.age).toEqual(1);
-  })
-
-  it('throws an error if the pet is not alive', () => {
-  const pet = new Pet('Kalb');
-  pet.age = 30;
-
-  expect(pet.growUp()).toThrow('Your pet is no longer alive!')
-  })
-  });
-
 describe('checkUp', () => {
-  it('returns I need a walk if fitness is less than 3', () => {
+
+  it('returns I need a walk if fitness is less than 3', () => {  
+    const pet = new Pet('Kalb');
     pet.fitness = 3;
     pet.checkUp();
 
@@ -107,22 +112,24 @@ describe('checkUp', () => {
   });
 
     it('returns I am hungry! if hunger is greater than or equal to 5', () => {
-
+    const pet = new Pet('Kalb');
     pet.hunger = 0;
     pet.fitness = 3;
   
-    expect(pet.checkUp()).toBe('I am hungry');
+    expect(pet.checkUp()).toBe('I need a walk!');
   });
 
     it('returns I am hungry! and I need a walk if fitness is 3 or less AND hunger is 5 or more', () => {
+      const pet = new Pet('Kalb');
       pet.hunger = 5;
       pet.fitness = 3;
       pet.checkUp();
       
-      expect(pet.checkUp()).toBe('I am hungry AND I need a walk');
+      expect(pet.checkUp()).toBe('I am hungry AND I need a walk!');
     }),
     
     it('if neither fitness or hunger are true then return I feel great!', () => {
+      const pet = new Pet('Kalb');
       pet.fitness = 4;
       pet.hunger = 4;
       pet.checkUp();
@@ -133,6 +140,6 @@ describe('checkUp', () => {
     const pet = new Pet ('Kalb');
     pet.age = 30;
 
-    expect(() => pet.checkUp()).toThrow('Your pet is no longer alive');
+    expect(() => pet.checkUp()).toThrow('Your pet is no longer alive!');
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -5,7 +5,6 @@ const AGE_AT_BIRTH = 0;
 const AGE_INCREMENT = 1;
 const MAXIMUM_AGE = 30;
 const MAXIMUM_HUNGER = 10;
-const MINIMUM_HUNGER = 0;
 const NOT_HUNGRY = 0;
 
 function Pet(name) {
@@ -17,49 +16,54 @@ function Pet(name) {
 
 Pet.prototype = {
   get isAlive() {
-    return this.age < MAXIMUM_AGE && this.hunger < 10 && this.fitness > 0;
+    return (
+      this.age < MAXIMUM_AGE &&
+      this.hunger < MAXIMUM_HUNGER &&
+      this.fitness > MINIMUM_FITNESS
+    );
   }
-}
+};
 
 Pet.prototype.growUp = function () {
-  if(this.isAlive) {
-    throw new Error('Your pet is no longer alive');
+  if(!this.isAlive) {
+    throw new Error('Your pet is no longer alive!')
   }
   this.age += AGE_INCREMENT;
   this.hunger += 5;
   this.fitness -= 3;
+
 };
 
   Pet.prototype.walk = function() {
-    if(this.isAlive) {
-      throw new error('Your pet is no longer alive!')
+    if(!this.isAlive) {
+      throw new Error('Your pet is no longer alive!')
     }
       this.fitness = Math.min(this.fitness + FITNESS_INCREASE, MAXIMUM_FITNESS);
   };
 
-  Pet.prototype.walk = function() {
-    if(this.isAlive) {
-      throw new error('Your pet is no longer alive!')
-    }
-    this.age += AGE_INCREMENT;
-    this.hunger += 5;
-    this.fitness -= 3;
+  Pet.prototype.feed = function () {
+    if (!this.isAlive) {
+      throw new Error('Your pet is no longer alive!')
+    };
+        this.hunger = Math.max(this.hunger - 3, NOT_HUNGRY)
   };
 
-  Pet.prototype.checkUp = function() { 
-    const isHungry = (this.hunger >= 5);
-    const needsWalk = (this.fitness <= 3);
-  
-    if (this.fitness <= 3 && this.hunger >= 5)
-    { 
-      return "I am hungry AND I need a walk";
-    };
+  Pet.prototype.checkUp = function() {
+    if(!this.isAlive) {
+      throw new Error('Your pet is no longer alive!');
+    }
+
+    if (this.fitness <= 3 && this.hunger >= 5) {
+      return "I am hungry AND I need a walk!";
+    }
     if (this.fitness <= 3) {
-      return "I need a walk";
+      return "I need a walk!";
+    }
+
+    if (this.hunger <= 3) {
+      return "I am hungry!"
     };
-    if (isHungry) {
-      return "I am hungry"
-    };
+
       return "I feel great!"
   };
 
